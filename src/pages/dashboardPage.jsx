e3716a11af23cda76d98ea8964ff43ec63f482cb
Varun R <r.varun9560@gmail.com>
2025-07-04 02:09:46 +0530

    New File on app.jsx

--- src/pages/dashboardPage.jsx
+++ src/pages/dashboardPage.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import api from '../services/api'
 
-function DashboardPage() {
+function dashboardPage() {
 
   const [tasks , setTasks] = useState([])
   const [filterStatus , setFilterStatus] = useState('all')
@@ -168,4 +168,4 @@ function DashboardPage() {
   )
 }
 
-export default DashboardPage
+export default dashboardPage
